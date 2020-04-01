c13acbbfa8f3e7666c0530df63cc4e9bf57f6a53
Nicolay Makukhin <nnmakukhin@gmail.com>
2020-04-01 19:57:05 +0300

    Ex2 + menu

--- Lesson4/main.c
+++ Lesson4/main.c
@@ -20,12 +20,22 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <conio.h>
 #include <locale.h>
 #include <string.h>
 
+#define MENU_SIZE 4
+
+// структура для организации меню
+typedef struct MENU_ITEM_T
+{
+	void (*func)();
+	char* text;
+} MenuItem;
+
 int** createMatrix(int columns, int rows)
 {
-	char** m = (int**)malloc(columns * sizeof(int*));
+	int** m = (int**)malloc(columns * sizeof(int*));
 	for (int i = 0; i < columns; i++)
 		m[i] = (int*)malloc(rows * sizeof(int));
 	return m;
@@ -47,6 +57,7 @@ void printMatrix(int** m, int columns, int rows)
 		printf("\n");
 	}
 }
+
 
 int lcs_len(char* s1, char* s2)
 {
@@ -71,18 +82,106 @@ int lcs_len(char* s1, char* s2)
 	return len;
 }
 
-int main(int argc, char** argv)
+int count_paths(int** m, int i, int j)
 {
-	setlocale(LC_ALL, "Russian");
+	if (i == 0 && j > 0)
+		return 1;
+	if (i > 0 && j == 0)
+		return 1;
 
-	// 2
+	if (m[i][j] == 0)
+		return 0;
+
+	return count_paths(m, i - 1, j) + count_paths(m, i, j - 1);
+}
+
+void Ex1()
+{
+	FILE* file;
+	
+	errno_t err = fopen_s( &file, "data.txt", "r");
+	if (err)
+		return;
+
+	int columns, rows;
+
+	fscanf_s(file, "%d %d", &columns, &rows);
+
+	int** m = createMatrix(columns, rows);
+
+	for (int i = 0; i < columns; i++)
+		for (int j = 0; j < rows; j++)
+			fscanf_s(file, "%d", &m[i][j]);
+
+	
+	fclose(file);
+
+	printf("Карта:\n");
+	printMatrix(m, columns, rows);
+
+	int count = count_paths(m, columns - 1, rows - 1);
+	
+	deleteMatrix(m, columns, rows);
+
+	printf("Количество маршрутов: %d\n", count);
+}
+
+void Ex2()
+{
 	char* seq1 = "geekbrains";
 	char* seq2 = "geekminds";
 
 	printf("sequence 1: %s\n", seq1);
 	printf("sequence 2: %s\n", seq2);
 	printf("lcs_len = %d\n", lcs_len(seq1, seq2));
+}
 
+void Ex3()
+{
+	printf("== Ex3 ==");
+}
+
+int main(int argc, char** argv)
+{
+	int selectedIndex;
+	MenuItem currentItem;
+
+	setlocale(LC_ALL, "Russian");
+
+	MenuItem menu[MENU_SIZE] = {
+		{ Ex1, "Индекс массы тела" },
+		{ Ex2, "Максимум из четырех чисел" },
+		{ Ex3, "Обмен значениями переменных" },
+		{ 0, "Выход" }
+	};
+
+
+	while (1)
+	{
+		system("cls");
+
+		for (int i = 0; i < MENU_SIZE; i++)
+			printf("%-4d%s\n", i + 1, menu[i].text);
+
+		printf("\nВАШ ВЫБОР: ");
+		if (scanf_s("%d", &selectedIndex) != 1)
+			continue;
+
+		// 1..15 -> 0..14
+		selectedIndex--;
+
+		if (selectedIndex >= 0 && selectedIndex < MENU_SIZE)
+		{
+			currentItem = menu[selectedIndex];
+			if (currentItem.func == 0)
+				return 0;
+
+			currentItem.func();
+
+			printf("\n\nНажмите любую клавишу для продолжения");
+			_getch();
+		}
+	}
 
 	return 0;
 }
